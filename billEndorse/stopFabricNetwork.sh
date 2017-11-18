638ae7b4e592a4fd424fad54bd9af2d4fe618de1
jinyu18 <jin-yu18@126.com>
2017-11-18 20:55:18 +0800

    add stop

---
+++ billEndorse/stopFabricNetwork.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function dkcl(){
+        CONTAINER_IDS=$(docker ps -aq)
+	echo
+        if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
+                echo "========== No containers available for deletion =========="
+        else
+                docker rm -f $CONTAINER_IDS
+        fi
+	echo
+}
+
+function dkrm(){
+        DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+	echo
+        if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+		echo "========== No images available for deletion ==========="
+        else
+                docker rmi -f $DOCKER_IMAGE_IDS
+        fi
+	echo
+}
+
+function restartNetwork() {
+	echo
+
+        #teardown the network and clean the containers and intermediate images
+	cd artifacts
+	docker-compose down
+	dkcl
+	dkrm
+
+	#Cleanup the material
+	rm -rf /tmp/hfc-test-kvs_peerOrg* $HOME/.hfc-key-store/ /tmp/fabric-client-kvs_peerOrg*
+
+	cd -
+	echo
+}
+
+
+
+
+restartNetwork
+
+killall node
